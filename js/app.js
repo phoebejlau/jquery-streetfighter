86e336f19b2d5695d58950bf29dc5bead50b2a22
Phoebe Lau <phoebejlau.la@gmail.com>
2016-08-14 12:59:33 -0700

    hadouken sound added

--- js/app.js
+++ js/app.js
@@ -8,11 +8,18 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
-    // play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide()
     $('.ryu-throwing').show()
-    $('.hadouken').show()
-    // animate it to the right of the screen    
+    $('.hadouken').finish().show()
+    .animate(
+      {'left': '1020'},
+      500,
+      function() {
+        $('.hadouken').hide();
+        $('.hadouken').css('left', '520px');
+      }
+  );  
   })
   .mouseup(function() {
     // ryu goes back to his ready position
@@ -20,3 +27,10 @@ $(document).ready(function() {
     $('.ryu-ready').show()
   });
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+
+}
